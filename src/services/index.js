f5fd95728d0250b49cfdb164f502d89a8b078338
learningreac <learningreact2020@gmai.com>
2022-04-17 20:07:29 -0700

    Communication with the Backend to a single Module

--- src/services/index.js
+++ src/services/index.js
@@ -2,13 +2,19 @@ import axios from "axios";
 const personsUrl = "http://localhost:3001/persons";
 const messagesUrl = " http://localhost:3001/messages";
 
-const getLoginUserData = async () => {
+const getAllUser = async () => {
     const request = await axios.get(personsUrl);
     return request.then(response => response.data);
 }
 
-const getloginUserMsgs = async () => {
-    const response = axios.get(messagesUrl)
+const getAllMsgs = async () => {
+    const request = await axios.get(messagesUrl);
+    return request.then(response => response.data);
 };
 
-export default { getLoginUserData }
+const createMsg = async (newObject) => {
+    const request = axios.post(messagesUrl, newObject);
+    return request.then(response => response.data);
+}
+
+export default { getAllUser, getAllMsgs, createMsg };
